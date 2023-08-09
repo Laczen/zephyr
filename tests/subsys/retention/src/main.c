840eb576cc16e73545e29a728edd3a3e1b099163
Laczen JMS <laczenjms@gmail.com>
2023-08-09 15:23:58 +0200

    retention subsystem add test for comparison
    Add a test to allow comparison between the boot_info and retention subsystem.
    The result of the comparison is that retention add 2300 bytes to the code
    without having any real advantage.
    
    Rom size for boot_info: 20461 byte
    Rom size for retention: 22761 byte
    
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>

---
+++ tests/subsys/retention/src/main.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+#include <zephyr/retention/retention.h>
+
+static const struct device *ret_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_boot_mode));
+
+ZTEST_USER(retention_api, test_get_size)
+{
+	size_t bi_size = retention_size(ret_dev);
+
+	zassert_not_equal(bi_size, 0U, "Get size returned invalid value");
+}
+
+ZTEST_USER(retention_api, test_get_set)
+{
+	uint8_t wr[retention_size(ret_dev)];
+	uint8_t rd[retention_size(ret_dev)];
+	int rc = 0;
+
+	memset(wr, 0, sizeof(wr));
+	memset(rd, 0, sizeof(rd));
+
+	rc = retention_read(ret_dev, 0, wr, sizeof(wr));
+	zassert_equal(rc, 0, "retention_read returned [%d]", rc);
+
+	memset(wr, 0xa, sizeof(wr));
+	rc = retention_write(ret_dev, 0, wr, sizeof(wr));
+	zassert_equal(rc, 0, "retention_write returned [%d]", rc);
+
+	rc = retention_read(ret_dev, 0, rd, sizeof(rd));
+	zassert_equal(rc, 0, "retention_read returned [%d]", rc);
+
+	zassert_equal(memcmp(rd, wr, sizeof(wr)), 0, "data mismatch");
+}
+
+ZTEST_SUITE(retention_api, NULL, NULL, NULL, NULL, NULL);
