a9eb46fb3a5a7026f789ed309cde6661e03d2e39
Laczen JMS <laczenjms@gmail.com>
2023-08-11 17:23:17 +0200

    Added test to hello_world
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,22 @@
 
 #include <zephyr/kernel.h>
 
+#ifdef CONFIG_RETENTION
+#include <zephyr/retention/retention.h>
+
+static const struct device *ret_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_boot_mode));
+#endif
+
 int main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+#ifdef CONFIG_RETENTION
+	uint8_t dummy[3];
+	size_t bi_size = retention_size(ret_dev);
+	int rc = retention_read(ret_dev, 0, dummy, sizeof(dummy));
+
+	rc = retention_write(ret_dev, 0, dummy, sizeof(dummy));
+#endif
+
 	return 0;
 }
